76540315abc0572bd3ad50d7cfaeb44938063dfe
jayporta <jasonmatthewporta@gmail.com>
2019-09-25 22:42:46 +0800

    Removed CMapReaderFactory and added disableFontFace: false

--- src/Pdfvuer.vue
+++ src/Pdfvuer.vue
@@ -28,28 +28,28 @@ function isPDFDocumentLoadingTask(obj) {
 function createLoadingTask(src, options) {
 	var source;
 	if ( typeof(src) === 'string' )
-		source = { url: src };
+		source = { url: src, disableFontFace: false };
 	else
 	if ( typeof(src) === 'object' && src !== null )
-		source = Object.assign({}, src);
+		source = Object.assign({}, src, { disableFontFace: false });
 	else
 		throw new TypeError('invalid src type');
 
 	// see https://github.com/mozilla/pdf.js/blob/628e70fbb5dea3b9066aa5c34cca70aaafef8db2/src/display/dom_utils.js#L64
-	source.CMapReaderFactory = function() {
+	// source.CMapReaderFactory = function() {
 
-		this.fetch = function(query) {
+	// 	this.fetch = function(query) {
 
-			return import('raw-loader!pdfjs-dist/cmaps/'+query.name+'.bcmap' /* webpackChunkName: "noprefetch-[request]" */)
-			.then(function(bcmap) {
+	// 		return import('raw-loader!pdfjs-dist/cmaps/'+query.name+'.bcmap' /* webpackChunkName: "noprefetch-[request]" */)
+	// 		.then(function(bcmap) {
 
-				return {
-					cMapData: bcmap,
-					compressionType: pdfjsLib.CMapCompressionType.BINARY,
-				};
-			});
-		}
-	};
+	// 			return {
+	// 				cMapData: bcmap,
+	// 				compressionType: pdfjsLib.CMapCompressionType.BINARY,
+	// 			};
+	// 		});
+	// 	}
+	// };
 
 
 	var loadingTask = pdfjsLib.getDocument(source);
